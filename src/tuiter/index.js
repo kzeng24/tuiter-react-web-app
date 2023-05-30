4a4e15285229385d8f6fd7dc9231ec783d798d09
kzeng24 <kathyzeng2002@gmail.com>
2023-05-30 19:28:56 -0400

    making tuiter page dynamic

--- src/tuiter/index.js
+++ src/tuiter/index.js
@@ -13,10 +13,10 @@ function Tuiter() {
     <div>
       <Nav />
       <div className="row">
-        <div className="col-2">
+        <div className="col-sm-2 col-lg-1 col-xl-2">
           <NavigationSidebar />
         </div>
-        <div className="col-7">
+        <div className="col-sm-10 col-lg-7 col-xl-7">
           <Routes>
             <Route path="/home" element={<HomeScreen />} />
             <Route path="/explore" element={<ExploreScreen />} />
@@ -24,7 +24,7 @@ function Tuiter() {
             <Route path="/profile" element={<ProfileScreen />} />
           </Routes>
         </div>
-        <div className="col-3">
+        <div className="d-sm-none d-lg-block col-lg-4 col-xl-3">
           <WhoToFollowList />
         </div>
       </div>
--- src/tuiter/navigation-sidebar/index.js
+++ src/tuiter/navigation-sidebar/index.js
@@ -1,18 +1,45 @@
 import React from "react";
 import {Link, useLocation} from "react-router-dom";
+import {BsTwitter} from "react-icons/bs";
+import {BsFillHouseDoorFill} from "react-icons/bs";
+import { RiNotification4Fill } from "react-icons/ri";
+import {MdEmail} from "react-icons/md";
+import {BsFillBookmarkFill} from "react-icons/bs";
+import {GoListUnordered} from "react-icons/go";
+import { GoPerson } from "react-icons/go";
+import {FiMoreHorizontal} from "react-icons/fi";
 
 const NavigationSidebar = () => {
     const {pathname} = useLocation();
     const [ignore, tuiter, active] = pathname.split("/");
-    const links = ["home", "explore", "notifications", "messages", "bookmarks", "lists", "profile", "more"]
+    const links = [
+      { text: "home", icon: <BsTwitter size={20} /> },
+      { text: "explore", icon: <BsFillHouseDoorFill size={20} /> },
+      { text: "notifications", icon: <RiNotification4Fill size={20} /> },
+      { text: "messages", icon: <MdEmail size={20} /> },
+      { text: "bookmarks", icon: <BsFillBookmarkFill size={20} /> },
+      { text: "lists", icon: <GoListUnordered size={20} /> },
+      { text: "profile", icon: <GoPerson size={25} /> },
+      { text: "more", icon: <FiMoreHorizontal size={20} /> },
+    ];
+
   return (
-    <div className="list-group">
-        {links.map((link) =>
-            <Link to={`/tuiter/${link}`} className={`list-group-item text-capitalize ${active === link ? "active" : ""}`} >
-                {link}
-            </Link>
-        )}
-    </div>
+    <>
+      <div className="list-group">
+        {links.map((link) => (
+          <Link
+            to={`/tuiter/${link.text}`}
+            className={`list-group-item text-capitalize d-inline-flex ${
+              active === link.text ? "active" : ""
+            }`}
+          >
+            <span className="pe-2">{link.icon}</span>
+            <span className="d-none d-xl-block">{link.text}</span>
+          </Link>
+        ))}
+        <button className="btn btn-primary mt-2 p-2 rounded-pill">Tweet</button>
+      </div>
+    </>
   );
 };
 export default NavigationSidebar;
