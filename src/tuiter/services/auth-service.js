5043ac057f0e5133e5bdc8ce2ee9f364e4c6547f
kzeng24 <kathyzeng2002@gmail.com>
2023-06-12 13:50:20 -0400

    changed env

--- src/tuiter/services/auth-service.js
+++ src/tuiter/services/auth-service.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 // get URL to remote API
-const SERVER_API_URL = process.env.REACT_APP_SERVER_API_URL;
+const SERVER_API_URL = process.env.REACT_APP_API_BASE;
 const USERS_URL = `${SERVER_API_URL}/users`;
 
 // configure axios to suppport cookies for passing credentials
